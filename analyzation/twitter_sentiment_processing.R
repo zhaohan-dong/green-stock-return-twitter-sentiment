0adfa1a675db8968b83bb811d3996aa9cf8cbc2e
Zhaohan Dong <zhaohan_dong@94.143.dyn.lse.ac.uk>
2022-06-07 15:12:52 +0000

    update

--- analyzation/twitter_sentiment_processing.R
+++ analyzation/twitter_sentiment_processing.R
@@ -28,5 +28,12 @@ tweet_df <- tweet_df %>%
   filter((grepl("climate change|#climatechange|global warming|#globalwarming", text, ignore.case = TRUE) |
            grepl("climate change|#climatechange|global warming|#globalwarming", text, ignore.case = TRUE)))
 
+# Substitute [at]username with anonymous
+tweet_df$text <- gsub("@[a-zA-Z0-9]*", "@anonymous", tweet_df$text)
+tweet_df$quoted_text <- gsub("@[a-zA-Z1-9 ]*", "@anonymous", tweet_df$quoted_text)
 
+# Process through vader
+tweet_df$sentiment <- vader_df(tweet_df$text, neu_set=T)$compound
 
+# Save file
+save_as_csv(tweet_df, "data/tweet_sentiment.csv")
