68f7e3f13406d70917fd39e7e3ac63990b735605
Zhaohan Dong <zhaohan_dong@75.143.dyn.lse.ac.uk>
2022-06-09 18:41:15 +0000

    Update

--- analyzation/3_equity_portfolio_processing.R
+++ analyzation/3_equity_portfolio_processing.R
@@ -2,9 +2,29 @@
 
 require("tidyverse", quietly = TRUE)
 
-clean_stock <- read.csv("data/equity/clean_selected.csv")
+# Define function to cleanse data
+cleanse_stock_data <- function(df) {
+  result_df <- df %>%
+    pivot_longer(cols = !c(ticker, data_field),
+                 names_to ="date",
+                 values_to = "value",
+                 values_ptypes = numeric()) %>%
+    mutate(date = as.Date(date, "%d/%m/%Y"),
+           ticker = sub(" US Equity", "", ticker))
+  return(result_df)
+}
 
-clean_stock <- clean_stock %>%
-  mutate(across(everything(), as.character)) %>%
-  pivot_longer(cols = !Ticker,
-               names_to = "value")
+clean_stock <- read_csv("data/equity/clean_selected.csv") %>%
+  cleanse_stock_data() %>%
+  write_csv("data/equity/clean_cleansed.csv")
+rm(clean_stock)
+
+oil_gas_stock <- read_csv("data/equity/oil_gas_selected.csv") %>%
+  cleanse_stock_data() %>%
+  write_csv("data/equity/oil_gas_cleansed.csv")
+rm(oil_gas_stock)
+
+utility_stock <- read_csv("data/equity/utility_selected.csv") %>%
+  cleanse_stock_data() %>%
+  write_csv("data/equity/utility_cleansed.csv")
+rm(utility_stock)
