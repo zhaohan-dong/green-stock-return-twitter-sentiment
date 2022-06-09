9be6c10cc73da1c892945d386a6e98c510bcca5c
Zhaohan Dong <zhaohan_dong@198.207.dyn.lse.ac.uk>
2022-06-09 15:38:33 +0000

    Initial commit

--- analyzation/3_equity_portfolio_processing.R
+++ analyzation/3_equity_portfolio_processing.R
@@ -1,2 +1,10 @@
 ## We will cleanse the equity daily closing data
 
+require("tidyverse", quietly = TRUE)
+
+clean_stock <- read.csv("data/equity/clean_selected.csv")
+
+clean_stock <- clean_stock %>%
+  mutate(across(everything(), as.character)) %>%
+  pivot_longer(cols = !Ticker,
+               names_to = "value")
