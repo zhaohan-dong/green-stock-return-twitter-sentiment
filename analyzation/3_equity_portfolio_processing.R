7dee0edb8772dd6cfd79fc5d596bd8d773842e5e
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-06-10 11:29:18 +0000

    update

--- analyzation/3_equity_portfolio_processing.R
+++ analyzation/3_equity_portfolio_processing.R
@@ -16,15 +16,19 @@ cleanse_stock_data <- function(df) {
 
 clean_stock <- read_csv("data/equity/clean_selected.csv") %>%
   cleanse_stock_data() %>%
-  write_csv("data/equity/clean_cleansed.csv")
-rm(clean_stock)
+  mutate(category = "clean")
 
 oil_gas_stock <- read_csv("data/equity/oil_gas_selected.csv") %>%
   cleanse_stock_data() %>%
-  write_csv("data/equity/oil_gas_cleansed.csv")
-rm(oil_gas_stock)
+  mutate(category = "oil-gas")
 
 utility_stock <- read_csv("data/equity/utility_selected.csv") %>%
   cleanse_stock_data() %>%
-  write_csv("data/equity/utility_cleansed.csv")
-rm(utility_stock)
+  mutate(category = "utility")
+
+price <- rbind(clean_stock, oil_gas_stock, utility_stock) %>%
+  filter(data_field == "PX_LAST") %>%
+  group_by(ticker) %>%
+  mutate(lag1 = lag(value, n = 1L, order_by = date)) %>%
+  mutate(raw_return = (value - lag1) / lag1) %>%
+  select(!lag1)
