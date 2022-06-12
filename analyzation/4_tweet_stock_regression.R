a222a3c9f1f32455c4cc69944241ef6b220cbf13
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-06-12 21:24:12 +0000

    update

--- analyzation/4_tweet_stock_regression.R
+++ analyzation/4_tweet_stock_regression.R
@@ -1,9 +1,10 @@
 ## Regression between tweets and stock
 
 require(tidyverse, quietly = TRUE)
+require(rtweet, quietly = TRUE)
 
 # Load data
-twitter_df <- read_csv("data/tweet_sentiment.csv")
+twitter_df <- read_twitter_csv("data/tweet_sentiment.csv")
 stock_equal_weight_raw <- read_csv("data/stock_equal_weight_raw.csv")
 stock_equal_weight_ff <- read_csv("data/stock_equal_weight_ff.csv")
 wx_df <- read_csv("data/KNYC_monthly_summary_processed.csv")
