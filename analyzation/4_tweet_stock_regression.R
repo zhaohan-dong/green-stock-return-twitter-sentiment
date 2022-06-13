1a87efc8ce7a04dcb817b69c4d62eb8cd0103614
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-06-13 15:20:54 +0000

    Update

--- analyzation/4_tweet_stock_regression.R
+++ analyzation/4_tweet_stock_regression.R
@@ -5,10 +5,12 @@ require(rtweet, quietly = TRUE)
 
 # Load data
 twitter_df <- read_twitter_csv("data/tweet_sentiment.csv")
-stock_equal_weight_raw <- read_csv("data/stock_equal_weight_raw.csv")
-stock_equal_weight_ff <- read_csv("data/stock_equal_weight_ff.csv")
+stock_equal_weight_raw <- read_csv("data/stock_equal_weight_raw.csv") %>%
+  mutate(date = as.Date(date))
+stock_equal_weight_ff <- read_csv("data/stock_equal_weight_ff.csv") %>%
+  mutate(date = as.Date(date))
 wx_df <- read_csv("data/KNYC_monthly_summary_processed.csv") %>%
-  mutate(DATE = as.Date(DATE))
+  mutate(date = as.Date(date))
 oil_df <- read_csv("data/Cushing_OK_WTI_Spot_Price_FOB.csv")
 
 twitter_df <- twitter_df %>%
@@ -17,15 +19,20 @@ twitter_df <- twitter_df %>%
   select(date, status_id, retweet_count, quote_count, reply_count, sentiment) %>%
   filter(date >= as.Date("2011-04-01"))
 
-twitter_df <- twitter_df %>%
+twitter_summary <- twitter_df %>%
   group_by(date) %>%
-  summarize(across(sentiment, lst(mean, sd)))
+  summarize(across(sentiment, list(mean = ~ mean(.x, na.rm = TRUE), sd = ~ sd(.x, na.rm = TRUE)))) %>%
+  mutate(count(twitter_df, date, name = "daily_count"))
 
-
-ggplot(data=twitter_df, aes(x=date, y=sentiment_mean, group=1)) +
+ggplot(data=twitter_summary, aes(x=date, y=sentiment_mean, group=1)) +
   geom_line()+
   geom_point()
 
-ggplot(data=wx_df, aes(x=DATE, y=ab_temp, group=1)) +
+ggplot(data=twitter_summary, aes(x=date, y=daily_count, group=1)) +
   geom_line()+
   geom_point()
+
+ggplot(data=wx_df, aes(x=date, y=ab_temp, group=1)) +
+  geom_line()+
+  geom_point()
+
