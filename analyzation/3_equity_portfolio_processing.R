89ba4108b39ab753b488713a003199a2854ac088
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-06-23 20:31:19 +0000

    update with zoo package

--- analyzation/3_equity_portfolio_processing.R
+++ analyzation/3_equity_portfolio_processing.R
@@ -6,6 +6,8 @@
 require(tidyverse, quietly = TRUE)
 require(tidyquant, quietly = TRUE)
 require(broom, quietly = TRUE)
+require(PMwR, quietly = TRUE)
+require(zoo, quietly = TRUE)
 
 ## Define functions
 # Cleanse data input
@@ -21,7 +23,7 @@ cleanse_stock_data <- function(df) {
 }
 
 # Create dataframes for equal weighted portfolio from cleansed csv
-equal_weight_portfolio <- function(df) {
+create_portfolio <- function(df) {
   # Select and tidy data
   result_df <- df %>%
     filter(data_field == "PX_LAST") %>%
@@ -30,13 +32,6 @@ equal_weight_portfolio <- function(df) {
                 values_from = value) %>%
     # Replace NA values with previous value from column (same ticker)
     na.locf()
-  
-  # Calculate return
-  result_df <- result_df %>%
-    mutate(price = rowSums(result_df %>% select(!c(date, category))) /
-             (ncol(result_df) - 2)) %>%
-    # Add month helper column
-    mutate(yr_mon = format(date, "%Y-%m"), .after = date)
   return(result_df)
 }
 
@@ -57,37 +52,6 @@ equal_weight_portfolio <- function(df) {
 #   
 #   return(result_df)
 # }
-
-
-# Calculate daily raw return of portfolio from portfolio df
-daily_raw_return <- function(portfolio_df) {
-  result_df <- portfolio_df %>%
-    mutate(lag1 = lag(price, n = 1L, order_by = date)) %>%
-    mutate(daily_raw_return = (price - lag1) / lag1) %>%
-    select(!lag1) %>%
-    ungroup()
-  return(result_df)
-}
-
-# Calculate monthly raw return
-monthly_raw_return <- function(portfolio_df) {
-  result_df <- portfolio_df %>%
-    group_by(yr_mon) %>%
-    
-    # Select last day of month in the data
-    filter(date == max(date)) %>%
-    ungroup() %>%
-    
-    # Calculate monthly raw return
-    mutate(lag1 = lag(price)) %>%
-    mutate(monthly_raw_return = (price - lag1) / lag1) %>%
-    select(c(yr_mon, monthly_raw_return)) %>%
-    
-    # Merge resulting data with original dataframe
-    right_join(y = portfolio_df)
-  return(result_df)
-}
-
 
 # Combine fama-french three-factor and portfolio df with daily return
 # and calculate daily excess return + add year-month grouping helper column
@@ -148,31 +112,28 @@ volume <- function(df) {
 # Read stock data
 clean_stock <- read_csv("data/equity/clean_selected.csv") %>%
   cleanse_stock_data() %>%
-  mutate(category = "clean")
+  create_portfolio() %>%
+  read.zoo(FUN = as.Date)
 
 oil_gas_stock <- read_csv("data/equity/oil_gas_coal_selected.csv") %>%
   cleanse_stock_data() %>%
-  mutate(category = "oil-gas")
+  create_portfolio() %>%
+  read.zoo(FUN = as.Date)
 
 utility_stock <- read_csv("data/equity/utility_selected.csv") %>%
   cleanse_stock_data() %>%
-  mutate(category = "utility")
+  create_portfolio() %>%
+  read.zoo(FUN = as.Date)
 
 # Create equal weight portfolio and calculate daily raw return
 # Deleting individual stock ticker data
-clean_stock_equal_weight <- equal_weight_portfolio(clean_stock) %>%
-  daily_raw_return() %>%
-  monthly_raw_return() %>%
-  select(date, yr_mon, category, price, daily_raw_return, monthly_raw_return) %>%
-  filter(date != as.Date("2012-03-30")) # Delete head of data with no return info
-oil_gas_stock_equal_weight <- equal_weight_portfolio(oil_gas_stock) %>%
-  daily_raw_return() %>%
-  monthly_raw_return() %>%
-  select(date, yr_mon, category, price, daily_raw_return, monthly_raw_return) %>%
-  filter(date != as.Date("2012-03-30")) # Delete head of data with no return info
-utility_stock_equal_weight <- equal_weight_portfolio(utility_stock) %>%
-  daily_raw_return() %>%
-  monthly_raw_return() %>%
+clean_stock_daily_return <- returns(clean_stock)
+oil_gas_stock_equal_weight <- create_portfolio(oil_gas_stock) %>%
+  read.zoo(FUN = as.Date) %>%
+  returns()
+utility_stock_equal_weight <- create_portfolio(utility_stock) %>%
+  read.zoo(FUN = as.Date) %>%
+  returns()
   select(date, yr_mon, category, price, daily_raw_return, monthly_raw_return) %>%
   filter(date != as.Date("2012-03-30")) # Delete head of data with no return info
 
--- analyzation/4_tweet_stock_regression.R
+++ analyzation/4_tweet_stock_regression.R
@@ -104,13 +104,13 @@ rm(ff_data,
 ###################################################################
 
 # Create multiple linear regression model
-clean_price_model <- lm(log(price_mean) ~  log(monthly_count) + log(oil_price_mean), data = twitter_clean_summary)
+clean_price_model <- lm(log(price_mean) ~  monthly_count + oil_price_mean, data = twitter_clean_summary)
 summary(clean_price_model)
 autoplot(clean_price_model)
-oil_gas_price_model <- lm(log(price_mean) ~  log(monthly_count) + log(lag1_count) + log(oil_price_mean), data = twitter_oil_gas_summary)
+oil_gas_price_model <- lm(log(price_mean) ~  monthly_count + lag1_count + oil_price_mean, data = twitter_oil_gas_summary)
 summary(oil_gas_price_model)
 autoplot(oil_gas_price_model)
-bmg_price_model <- lm(log(price_mean) ~  log(monthly_count) + log(lag1_count) + log(oil_price_mean), data = twitter_bmg_summary)
+bmg_price_model <- lm(log(price_mean) ~  monthly_count + lag1_count + oil_price_mean, data = twitter_bmg_summary)
 summary(bmg_price_model)
 autoplot(bmg_price_model)
 
@@ -119,12 +119,15 @@ ggplot(twitter_clean_summary, aes(x = log(price_mean), y = log(monthly_count))) 
 
 
 # Return
-clean_return_model <- lm(daily_raw_return_mean ~  count_change_perc + oil_price_change_perc + Mkt_RF, data = twitter_clean_summary)
-oil_gas_return_model <- lm(daily_raw_return_mean ~  count_change_perc + oil_price_change_perc + Mkt_RF, data = twitter_oil_gas_summary)
-bmg_return_model <- lm(daily_raw_return_mean ~  count_change_perc + oil_price_change_perc + Mkt_RF, data = twitter_bmg_summary)
+clean_return_model <- lm(daily_raw_return_mean ~  monthly_count + oil_price_change_perc + Mkt_RF, data = twitter_clean_summary)
+oil_gas_return_model <- lm(daily_raw_return_mean ~  monthly_count + oil_price_change_perc + Mkt_RF, data = twitter_oil_gas_summary)
+bmg_return_model <- lm(daily_raw_return_mean ~  monthly_count + oil_price_change_perc + Mkt_RF, data = twitter_bmg_summary)
 summary(clean_return_model)
+autoplot(clean_return_model)
 summary(oil_gas_return_model)
+autoplot(clean_return_model)
 summary(bmg_return_model)
+autoplot(bmg_return_model)
 
 # # Can't do these
 # # Return standard deviation, the more people tweets, the more return sd it is
