b3b178b5602b6c0d700f1843fd95dac28f0aab40
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-07-01 10:51:41 +0000

    Green sentiment done

--- analyzation/green_sentiment_etf.R
+++ analyzation/green_sentiment_etf.R
@@ -7,21 +7,38 @@ require(broom, quietly = TRUE)
 require(rtweet, quietly = TRUE)
 require(readxl, quietly = TRUE)
 
+# All etf data
 etf_df <- read_xlsx("data/equity/all_etf.xlsx") %>%
   rename(date = `Data Date - Security Monthly`,
          ticker = `Ticker Symbol`,
          volume = `Trading Volume - Monthly`,
          price = `Price - Close - Monthly`,
          return = `Monthly Total Return`,
-         shares_out = `Shares Outstanding Monthly - Issue`,
-         nav = `Net Asset Value - Monthly`) %>%
-  mutate(date = floor_date(as.Date(date), "monthly")) %>%
-  select(date, ticker, price, volume, return, shares_out, nav) %>%
+         shares_out = `Shares Outstanding Monthly - Issue`) %>%
+  mutate(date = floor_date(as.Date(date), "monthly"),
+         shares_out = shares_out / 1000000) %>% # shares outstanding in millions (same as bloomberg data)
+  group_by(ticker) %>%
+  mutate(return = price / lag(price) - 1) %>%
+  ungroup() %>%
+  select(date, ticker, price, volume, return, shares_out) %>%
   mutate(flows = shares_out / lag(shares_out) - 1,
-         flows = gsub("Inf", NA, flows),
-         green = 0)
+         flows = gsub("Inf", NA, flows))
 
-green_etf_df <- read_csv("data/equity/fund_flow.csv") %>%
+etf_nav_df <- read_csv("data/equity/all_etf_nav.csv") %>%
+  pivot_longer(cols = !c(ticker, data_field),
+               names_to ="date",
+               values_to = "value") %>%
+  mutate(date = floor_date(as.Date(date, "%m/%d/%Y"), "monthly"),
+         ticker = sub(" US Equity", "", ticker)) %>%
+  pivot_wider(names_from = c(data_field), values_from = value) %>%
+  rename(nav = FUND_TOTAL_ASSETS) # Caution total asset != NAV
+
+etf_df <- etf_df %>%
+  merge(etf_nav_df) %>%
+  mutate(nav = nav / shares_out)
+
+# Green etf data
+green_etf_df <- read_csv("data/equity/green_etf_flow.csv") %>%
   pivot_longer(cols = !c(ticker, data_field),
                names_to ="date",
                values_to = "value",
@@ -34,8 +51,20 @@ green_etf_df <- read_csv("data/equity/fund_flow.csv") %>%
          fund_flow = FUND_FLOW,
          shares_out = EQY_SH_OUT,
          nav = FUND_TOTAL_ASSETS) %>%
-  mutate(flows = shares_out / lag(shares_out) - 1)
+  mutate(flows = shares_out / lag(shares_out) - 1,
+         green = 1,
+         nav = nav / shares_out,
+         return = price / lag(price) - 1) %>%
+  select(-fund_flow)
 
+etf_df <- etf_df %>% 
+  filter(!ticker %in% unique(green_etf_df$ticker)) %>%
+  mutate(green = 0) %>%
+  rbind(green_etf_df)
+
+rm(etf_nav_df, green_etf_df)
+
+# Perform cross sectional regression
 result_df <- data.frame()
 
 for (d in unique(etf_df$date)) {
@@ -48,8 +77,4 @@ result_df <- result_df %>%
   rename(date = d) %>%
   mutate(date = as.Date(date, origin = "1970-01-01")) %>%
   arrange(date)
-
-
-as_tibble(monthly_model$coefficients)
-summary(etf_model)
 
