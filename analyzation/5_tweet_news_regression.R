c9f2c15777648a284111b18563392a7315b213aa
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-07-02 10:45:32 +0000

    update

--- analyzation/5_tweet_news_regression.R
+++ analyzation/5_tweet_news_regression.R
@@ -5,3 +5,6 @@ require(readxl, quietly = TRUE)
 
 nyt_climate_change <- read_excel("data/nyt_climate_change_2008_2018.xlsx")
 nyt_global_warming <- read_excel("data/nyt_global_warming_2008_2018.xlsx")
+ap_climate_change <- read_excel("data/ap_climate_change_2008_2018.xlsx")
+ap_global_warming <- read_excel("data/ap_global_Warming_2008_2018.xlsx")
+
