428ebf66bebc6c961febdeb30f65b6ca15a76b03
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-07-02 10:41:38 +0000

    initial commit

--- analyzation/5_tweet_news_regression.R
+++ analyzation/5_tweet_news_regression.R
@@ -1,4 +1,7 @@
 ## Regression between tweets and NYT/AP
 
 require(tidyverse, quietly = TRUE)
+require(readxl, quietly = TRUE)
 
+nyt_climate_change <- read_excel("data/nyt_climate_change_2008_2018.xlsx")
+nyt_global_warming <- read_excel("data/nyt_global_warming_2008_2018.xlsx")
