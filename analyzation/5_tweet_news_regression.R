446879bad7ab8ecef3653dd06c77ff3a2e051ba8
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-07-11 09:49:29 +0000

    added ar model

--- analyzation/5_tweet_news_regression.R
+++ analyzation/5_tweet_news_regression.R
@@ -26,22 +26,22 @@ ap_global_warming <- read_excel("data/ap_global_Warming_2008_2018.xlsx") %>%
   mutate(date = floor_date(date, unit = "month"))
 
 nyt_climate_change <- nyt_climate_change %>%
-  merge(count(nyt_climate_change, date, name = "monthly_nyt_climate_change_count")) %>%
-  select(date, monthly_nyt_climate_change_count) %>%
+  merge(count(nyt_climate_change, date, name = "nyt_climate_change_count")) %>%
+  select(date, nyt_climate_change_count) %>%
   distinct() %>%
   filter(!is.na(date))
 nyt_global_warming <- nyt_global_warming %>%
-  merge(count(nyt_global_warming, date, name = "monthly_nyt_global_warming_count")) %>%
-  select(date, monthly_nyt_global_warming_count) %>%
+  merge(count(nyt_global_warming, date, name = "nyt_global_warming_count")) %>%
+  select(date, nyt_global_warming_count) %>%
   distinct() %>%
   filter(!is.na(date))
 ap_climate_change <- ap_climate_change %>%
-  merge(count(ap_climate_change, date, name = "monthly_ap_climate_change_count")) %>%
-  select(date, monthly_ap_climate_change_count) %>%
+  merge(count(ap_climate_change, date, name = "ap_climate_change_count")) %>%
+  select(date, ap_climate_change_count) %>%
   distinct()
 ap_global_warming <- ap_global_warming %>%
-  merge(count(ap_global_warming, date, name = "monthly_ap_global_warming_count")) %>%
-  select(date, monthly_ap_global_warming_count) %>%
+  merge(count(ap_global_warming, date, name = "ap_global_warming_count")) %>%
+  select(date, ap_global_warming_count) %>%
   distinct()
 
 news_summary_df <- nyt_climate_change %>%
@@ -49,10 +49,11 @@ news_summary_df <- nyt_climate_change %>%
   merge(ap_climate_change) %>%
   merge(ap_global_warming) %>%
   group_by(date) %>%
-  mutate(news_total = sum(monthly_nyt_climate_change_count,
-                          monthly_nyt_global_warming_count,
-                          monthly_ap_climate_change_count,
-                          monthly_ap_global_warming_count)) %>%
+  mutate(nyt_total = nyt_climate_change_count +
+           nyt_global_warming_count,
+         ap_total = ap_climate_change_count +
+           ap_global_warming_count,
+         news_total = nyt_total + ap_total) %>%
   ungroup()
 
 summary_df <- read_csv("data/combined_monthly_data.csv")
@@ -61,4 +62,21 @@ green_etf_factor <- read_csv("data/green_etf_factor.csv")
 df <- news_summary_df %>%
   merge(summary_df) %>%
   merge(green_etf_factor)
+
+tweet_model <- arima(df$monthly_tweet_count, order = c(1, 0, 0))
+nyt_model <- arima(df$nyt_total, order = c(1, 0, 0))
+ap_model <- arima(df$ap_total, order = c(1, 0, 0))
+news_model <- arima(df$news_total, order = c(1, 0, 0))
+
+df$tweet_residual <- c(tweet_model$residuals)
+df$nyt_residual <- c(nyt_model$residuals)
+df$ap_residual <- c(ap_model$residuals)
+df$news_residual <- c(news_model$residuals)
+
+p <- ggplot(df, aes(x = date))
+
+p + geom_line(aes(y = news_residual)) +
+  geom_line(aes(y = tweet_residual / 1000, color = "Blue"))
+
+cor.test(df$nyt_residual, lag(df$tweet_residual, 1), use = "complete.obs")
 
